fb2023f4d9118e7b056c626a7fbc948f5ddcbe72
amarjyothi98 <amarjyothi526@gmail.com>
2023-04-10 00:16:28 +0530

    initial pause on the team page

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './team.css'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import EmailIcon from '@mui/icons-material/Email';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 export default function Team() {
   let message = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. \n Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. `
@@ -18,7 +22,76 @@ export default function Team() {
                 <div className="col-sm-6 col-md-4">
                   <div className="team-item">
 
-                    <img src="/assets/turtle.png" alt="" className="team-img" />
+                    <img src="/assets/turtle.jpg" alt="" className="team-img" />
+                    <h3>UTKARSH SINGH</h3>
+                    <div className="team-info">
+                      <p>General Secretary</p>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsum?</p>
+
+                      <ul className="team-icon">
+                        <li><a href="" className='twitter'><LinkedInIcon/></a></li>
+                        <li><a href="" className='pinterest'><EmailIcon/></a></li>
+                        <li><a href="" className='facebook'><InstagramIcon/></a></li>
+                        <li><a href="" className='dribble'><AccessTimeIcon/></a></li>
+                      </ul>
+                    </div>
+
+                  </div>
+                </div>
+                <div className="col-sm-6 col-md-4">
+                  <div className="team-item">
+
+                    <img src="/assets/turtle.jpg" alt="" className="team-img" />
+                    <h3>UTKARSH SINGH</h3>
+                    <div className="team-info">
+                      <p>General Secretary</p>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsum?</p>
+
+                      <ul className="team-icon">
+                        <li><a href="" className='twitter'><LinkedInIcon/></a></li>
+                        <li><a href="" className='pinterest'><EmailIcon/></a></li>
+                        <li><a href="" className='facebook'><InstagramIcon/></a></li>
+                        <li><a href="" className='dribble'><AccessTimeIcon/></a></li>
+                      </ul>
+                    </div>
+
+                  </div>
+                </div>
+                <div className="col-sm-6 col-md-4">
+                  <div className="team-item">
+
+                    <img src="/assets/turtle.jpg" alt="" className="team-img" />
+                    <h3>UTKARSH SINGH</h3>
+                    <div className="team-info">
+                      <p>General Secretary</p>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsum?</p>
+
+                      <ul className="team-icon">
+                        <li><a href="" className='twitter'><LinkedInIcon/></a></li>
+                        <li><a href="" className='pinterest'><EmailIcon/></a></li>
+                        <li><a href="" className='facebook'><InstagramIcon/></a></li>
+                        <li><a href="" className='dribble'><AccessTimeIcon/></a></li>
+                      </ul>
+                    </div>
+
+                  </div>
+                </div>
+                <div className="col-sm-6 col-md-4">
+                  <div className="team-item">
+
+                    <img src="/assets/turtle.jpg" alt="" className="team-img" />
+                    <h3>UTKARSH SINGH</h3>
+                    <div className="team-info">
+                      <p>General Secretary</p>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsum?</p>
+
+                      <ul className="team-icon">
+                        <li><a href="" className='twitter'><LinkedInIcon/></a></li>
+                        <li><a href="" className='pinterest'><EmailIcon/></a></li>
+                        <li><a href="" className='facebook'><InstagramIcon/></a></li>
+                        <li><a href="" className='dribble'><AccessTimeIcon/></a></li>
+                      </ul>
+                    </div>
 
                   </div>
                 </div>
